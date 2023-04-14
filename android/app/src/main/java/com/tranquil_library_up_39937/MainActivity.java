1b4783e8126102e63696ec1c43242bb48c2a50c9
Crowdbotics <team@crowdbotics.com>
2023-04-14 15:38:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tranquil_library_up_39937/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tranquil_library_up_39937;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tranquil_library_up_39937";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tranquil-library-up-39937",
+  url: "https://tranquil-library-up-39937.botics.co",
+  api: "https://tranquil-library-up-39937.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
